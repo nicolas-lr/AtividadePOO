fc061e8522283b9a4f5a4911b8e239e5abb68a85
nicolas-lr <01618798@sempreuninassau.com.br>
2023-11-30 00:39:52 -0300

    Atividade POO

---
+++ Main.java
@@ -0,0 +1,10 @@
+// @author Nicolas
+import java.util.Scanner;
+public class Main {
+public static void main(String[] args){
+  Scanner scan = new Scanner(System.in);
+  scan.nextLine();
+  System.out.println("digite algo");
+  scan.close();
+}
+}
